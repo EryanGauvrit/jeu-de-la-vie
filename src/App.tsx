ba04bbf0530f89904109de64794c194d61e095b5
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2024-10-14 20:20:14 +0200

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import logo from './assets/logo.png';
 import Game from './components/Game';
 
 function App() {
     return (
         <main className="container flex flex-col items-center py-5 gap-10">
-            <h1 className="text-3xl uppercase font-bold">Le Jeu de la Vie</h1>
+            <div className="flex items-center gap-5">
+                <img src={logo} alt="Pixel Game Life Logo" className="w-10 h-10" />
+                <h1 className="text-3xl uppercase font-bold text-amber-500">Pixel Game Life</h1>
+            </div>
             <Game />
         </main>
     );
